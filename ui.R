f3d49b40b1f16d9a10d45b9feb30dea5b9596260
Beeley Chris - Senior Analyst <Chris.Beeley@nottshc.nhs.uk>
2019-11-03 19:28:04 +0000

    Added user and wordcloud tabs

--- ui.R
+++ ui.R
@@ -3,28 +3,47 @@ library(shinydashboard)
 
 dashboardPage(
     dashboardHeader(title = "NHS-R FOMO killer"),
-    dashboardSidebar(),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Tweets", tabName = "tweets", icon = icon("bird")),
+            menuItem("Users", tabName = "users", icon = icon("user")),
+            menuItem("Wordcloud", tabName = "wordcloud", icon = icon("cloud"))
+        )),
+    
     dashboardBody(
-        fluidRow(
-            
-            valueBoxOutput("numberOfTweets"),
-            
-            valueBoxOutput("mostFavourites")
-            
-        ),
         
-        fluidRow(
-            # most recent
-            
-            box(title = HTML("Ten most recent"), width = 6, 
-                solidHeader = TRUE,
-                status = "success",
-                htmlOutput("recentText")),
-            
-            box(title = "Ten most favourited", width = 6, 
-                solidHeader = TRUE,
-                status = "danger",
-                htmlOutput("favouriteText"))
+        tabItems(
+            tabItem("tweets",
+                    fluidRow(
+                        
+                        valueBoxOutput("numberOfTweets"),
+                        valueBoxOutput("mostFavourites")
+                    ),
+                    
+                    fluidRow(
+                        
+                        box(title = HTML("Ten most recent"), width = 6, 
+                            solidHeader = TRUE,
+                            status = "success",
+                            htmlOutput("recentText")),
+                        
+                        box(title = "Ten most favourited", width = 6, 
+                            solidHeader = TRUE,
+                            status = "danger",
+                            htmlOutput("favouriteText"))
+                    )
+            ),
+            tabItem("users",
+                    fluidRow(
+                        
+                        valueBoxOutput("numberOfUsers"),
+                        valueBoxOutput("topTweeter", width = 6)
+                    )
+            ),
+            tabItem("wordcloud",
+                        plotOutput("wordCloudPlot")
+
+            )
         )
     )
 )
