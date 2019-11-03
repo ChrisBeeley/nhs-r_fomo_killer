a4903faba00abace172daf4b78465af929a54c4e
Beeley Chris - Senior Analyst <Chris.Beeley@nottshc.nhs.uk>
2019-11-03 17:52:44 +0000

    Very basic shiny dashboard

--- server.R
+++ server.R
@@ -2,17 +2,52 @@
 library(rtweet)
 library(tidyverse)
 
-# tweets <- search_tweets(q = "rstats", n = 100)
+# tweets <- search_tweets(q = "NHSRconf2019", n = 100)
 # 
 # save(tweets, file = "tweetData.Rdata")
 
 load("tweetData.Rdata")
 
 function(input, output) {
-
-    output$tweetText <- renderText({
-        
-        paste0("<p>", tweets$screen_name, ": ", tweets$text, "</p>")
-    })
-
+  
+  output$numberOfTweets <- renderValueBox({
+    
+    tweet_df <- tweets %>% 
+      filter(created_at > Sys.time() - 60 * 60 * 24)
+    
+    valueBox(
+      nrow(tweet_df), HTML("Number of tweets in <br>last 24 hours"), icon = icon("clock"),
+      color = "green"
+    )
+  })
+  
+  output$mostFavourites <- renderValueBox({
+    
+    tweet_df <- tweets %>% 
+      filter(created_at > Sys.time() - 60 * 60 * 24)
+    
+    valueBox(
+      sum(tweet_df$favorite_count), 
+      HTML("Number of favourites<br>in last 24 hours"), icon = icon("clock"),
+      color = "red"
+      )
+  })
+  
+  output$recentText <- renderText({
+    
+    tweet_df <- tweets %>% 
+      arrange(desc(created_at)) %>% 
+      slice(1 : 10)
+    
+    paste0("<p>", tweet_df$screen_name, ": ", tweet_df$text, "</p>")
+  })
+  
+  output$favouriteText <- renderText({
+    
+    tweet_df <- tweets %>% 
+      arrange(desc(favourites_count)) %>% 
+      slice(1 : 10)
+    
+    paste0("<p>", tweet_df$screen_name, ": ", tweet_df$text, "</p>")
+  })
 }
--- ui.R
+++ ui.R
@@ -1,16 +1,30 @@
 
 library(shinydashboard)
 
-fluidPage(
-
-    titlePanel("NHS-R FOMO killer"),
-
-    sidebarLayout(
-        sidebarPanel(
+dashboardPage(
+    dashboardHeader(title = "NHS-R FOMO killer"),
+    dashboardSidebar(),
+    dashboardBody(
+        fluidRow(
+            
+            valueBoxOutput("numberOfTweets"),
+            
+            valueBoxOutput("mostFavourites")
+            
         ),
-
-        mainPanel(
-            htmlOutput("tweetText")
+        
+        fluidRow(
+            # most recent
+            
+            box(title = HTML("Ten most recent"), width = 6, 
+                solidHeader = TRUE,
+                status = "success",
+                htmlOutput("recentText")),
+            
+            box(title = "Ten most favourited", width = 6, 
+                solidHeader = TRUE,
+                status = "danger",
+                htmlOutput("favouriteText"))
         )
     )
 )
